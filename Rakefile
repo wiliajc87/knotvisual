388b9c8670662067cf6592126e0ec1e6732065fb
wiliajc87 <jordythepoet@gmail.com>
2015-01-29 15:33:56 -0600

    lightbox is working

--- Rakefile
+++ Rakefile
@@ -5,6 +5,7 @@ require ::File.expand_path('../config/environment', __FILE__)
 # Include all of ActiveSupport's core class extensions, e.g., String#camelize
 require 'active_support/core_ext'
 
+
 namespace :generate do
   desc "Create an empty model in app/models, e.g., rake generate:model NAME=User"
   task :model do
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,58 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('#block1').mouseenter(function() {
+    console.log('happened');
+    $( "#desc1" ).fadeIn("slow");
+  });
+  $('#block3').mouseenter(function() {
+    console.log('happened');
+    $( "#desc2" ).fadeIn("slow");
+  });
+  $('#block6').mouseenter(function() {
+    console.log('happened');
+    $( "#desc3" ).fadeIn("slow");
+  });
+  $('#block8').mouseenter(function() {
+    console.log('happened');
+    $( "#desc4" ).fadeIn("slow");
+  });
+  $('#block9').mouseenter(function() {
+    console.log('happened');
+    $( "#desc5" ).fadeIn("slow");
+  });
+  $('#block11').mouseenter(function() {
+    console.log('happened');
+    $( "#desc6" ).fadeIn("slow");
+  });
+    $('#block1').mouseleave(function() {
+    console.log('happened');
+    $( "#desc1" ).fadeOut("slow");
+  });
+  $('#block3').mouseleave(function() {
+    console.log('happened');
+    $( "#desc2" ).fadeOut("slow");
+  });
+  $('#block6').mouseleave(function() {
+    console.log('happened');
+    $( "#desc3" ).fadeOut("slow");
+  });
+  $('#block8').mouseleave(function() {
+    console.log('happened');
+    $( "#desc4" ).fadeOut("slow");
+  });
+  $('#block9').mouseleave(function() {
+    console.log('happened');
+    $( "#desc5" ).fadeOut("slow");
+  });
+  $('#block11').mouseleave(function() {
+    console.log('happened');
+    $( "#desc6" ).fadeOut("slow");
+  });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.gallery1').featherlightGallery();
+  $('.gallery2').featherlightGallery();
+  $('.gallery3').featherlightGallery();
+  $('.gallery4').featherlightGallery();
+  $('.gallery5').featherlightGallery();
+  $('.gallery6').featherlightGallery();
+
 });
